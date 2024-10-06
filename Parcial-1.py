9c49177a862370c4438d312fcbfce7833505ce3c
CamilaMailenLezcano <camilamailenlezcano@gmail.com>
2024-10-06 20:39:37 -0300

    Update Parcial-1.py

--- Parcial-1.py
+++ Parcial-1.py
@@ -3,4 +3,6 @@ def busca_primos(n):
     for num in range(2, 550):
         es_primo = True  
         for i in range (2,num): 
+            if num % i == 0: 
+                 es_primo = Fala
     
