0d537384727433ab090cad06b9e5b56dbe5483a3
uriermel <uriermel@gmail.com>
2024-10-06 20:58:06 -0300

    e

--- Parcial-1.py
+++ Parcial-1.py
@@ -2,7 +2,7 @@ def busca_primos(n):
 
     lista_p = [] # Creamos la lista, vacía, después le vamos a agregar cosas
     for num in range(2, 550): # Empezar en 2 porque 0 y 1 no son primos. vamos desde el 2 hasta el numero que pusimos (550). Pusimos que corte en 550 de forma adrede, para establecer un corte.
-        es_primo = True  # Después definiremos el False
+        es_primo = True  # Arrancamos estableciendo que todos los números son verdaderos para después poner que los números cuyo módulo es 0 los tome como falsos
         for i in range(2,num): 
             if num % i == 0: #Definimos el false (los números que no son primos)
                  es_primo = False 
