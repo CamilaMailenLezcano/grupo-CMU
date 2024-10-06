96c5e8e83cf7aeceea04b7b2a3fba269a5370346
MicaelaLabur98 <micaelalabur@gmail.com>
2024-10-06 20:51:20 -0300

    Update Parcial-1.py

--- Parcial-1.py
+++ Parcial-1.py
@@ -3,7 +3,7 @@ def busca_primos(n):
     lista_p = [] # Creamos la lista, vacía, después le vamos a agregar cosas
     for num in range(2, 550): # Empezar en 2 porque 0 y 1 no son primos. vamos desde el 2 hasta el numero que pusimos (550). Pusimos que corte en 550 de forma adrede, para establecer un corte.
         es_primo = True  # Después definiremos el False
-        for i in range (2,num): 
+        for i in range(2,num): 
             if num % i == 0: 
                  es_primo = False
         if es_primo:
